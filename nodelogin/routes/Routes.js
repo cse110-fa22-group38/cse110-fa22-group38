f199fc57d395104a4a06526a7b628f6c210a4e4b
Divyam Rana <divyamrana@gmail.com>
2022-11-11 15:01:58 -0800

    DB created

--- nodelogin/routes/Routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const fs = require('fs');
-const accountRoutes = require('./user_accounts.js')
-
-router.use(accountRoutes)
-module.exports = router;
