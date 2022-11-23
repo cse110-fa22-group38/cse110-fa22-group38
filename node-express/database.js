ecc49e9770fe6d8084267fb0ce6e8e0195c02203
tan002-fall2022 <114443774+tan002-fall2022@users.noreply.github.com>
2022-11-22 16:53:03 -0800

    Backend tung (#43)
    * login.js is created and login.html
    
    * Create 1.md
    
    * Update package-lock.json
    
    * Update package.json
    
    * Create passport-config.js
    
    * Update login.js
    
    * Rename API/1.md to API/views/index.html
    
    * Create login.html
    
    * Create register.html
    
    * Update login.html
    
    * database file
    
    * Making some changes
    
    * Delete index.js
    
    * We got the sites up running, but non-working functionalities
    Co-authored-by: Vedant2812 <Vedant2812@users.noreply.github.com>
    
    * A DEMO of a working CANVAS API
    
    * Updated the code a little bit
    
    * Refining the code a little bit to get them ready for database.js
    
    * First Draft
    
    * First Draft
    
    * Working database codes
    
    * Added databaseAPI.js to abstract the process of querrying from our database
    
    * Added a delete by username API to databaseAPI.js
    
    * Getting the login and the canvasAPI.js working
    
    * delete uuid = 343
    
    * not needed
    
    * Updated the database.js and the database ENDPOINTs
    
    * Implemented the codes for accountsettings, also the date format for canvasAPI so it should properly store in ISO8601 format now
    
    * delete the script for accountsettings
    
    * Fixed the format of start and end date in our database, and set the API endpoints to query dates based on today, this week and this month
    Co-authored-by: Vedant2812 <Vedant2812@users.noreply.github.com>
    
    * Updated the example of dataentry in database.js. Also added 3 endpoints in databaseAPI.js to query today, this week and this month.
    
    * Manually updated the source folder so that it maches with what frontend-node currently has
    
    * Manually updated public/js folder so that it maches with what frontend-node currently has
    
    * Manually updated public/js folder so that it matches with what frontend-node currently has
    
    * Adjusted today.html from frontend-node so that it displays correctly
    
    * not needed
    
    * not needed
    
    Co-authored-by: Divyam Rana <60857393+DivyamRana007@users.noreply.github.com>
    Co-authored-by: Divyam Rana <divyamrana@gmail.com>
    Co-authored-by: Vedant2812 <114892387+Vedant2812@users.noreply.github.com>

--- node-express/database.js
+++ node-express/database.js
@@ -6,7 +6,7 @@ const md5 = require('md5');
 let usersTable = 
 `
 CREATE TABLE IF NOT EXISTS users (
-  uuid PRIMARY KEY,
+
   username type UNIQUE,
   password_hash,
   api_token)
@@ -15,7 +15,9 @@ CREATE TABLE IF NOT EXISTS users (
 let eventTable = 
 `
 CREATE TABLE IF NOT EXISTS events (
-  uuid PRIMARY KEY,
+
+  username,
+
   event_id,
   event_type,
   event_name,
@@ -31,7 +33,8 @@ CREATE TABLE IF NOT EXISTS events (
 let insertNewUser = 
 `
 INSERT INTO users (
-  uuid,
+
+
   username,
   password_hash,
   api_token) VALUES (?,?,?,?)
@@ -68,15 +71,17 @@ module.exports = db;
 
 // Example of a dataentry object
 var dataentry = {
-    UUID: "userID",
-    DEID: "dataentryID",
-    type: "event",
-    name: "class 100a",
-    relation: "class 100a",
-    location: "9500 Gilman Drive",
-    details: "description",
-    start: "yyyy-mm-ddThh:mm:00",
-    end: "yyyy-mm-ddThh:mm:00",
-    done: Boolean(false),
-    color: "#ffffff"
+
+    "username": "Tung",
+    "event_id": "dataentryID",
+    "event_type": "event",
+    "event_name": "class 100a",
+    "event_relation": "class 100a",
+    "event_location": "9500 Gilman Drive",
+    "event_details": "description",
+    "event_start": "yyyy-mm-ddThh:mm:00Z",
+    "event_end": "yyyy-mm-ddThh:mm:00Z",
+    "event_completed": Boolean(false),
+    "event_color": "#ffffff"
+
 }
