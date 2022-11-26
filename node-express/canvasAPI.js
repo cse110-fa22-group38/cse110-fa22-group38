5d65ccbace626c7f96d21b3140ddf1c5421fe993
Tung <tan002@ucsd.edu>
2022-11-25 17:40:13 -0800

    Simplifying the process of grabbing only active courses from Canvas, now 100% accurate.

--- node-express/canvasAPI.js
+++ node-express/canvasAPI.js
@@ -8,16 +8,8 @@ const ical = require('ical.js');
 const db = require("./database.js");
 
 /* CANVAS API SECTION */
-// To store parsed data of all courses
-let data;
-
 // Storing the user's api token to fetch calendar events from Canvas
 let apiToken;
-
-// Some date constants
-const milliInDay = 1000 * 60 * 60 * 24;
-const daysInQuarter = 150;
-const todayDate = new Date();
 
 // Available type of events
 const REGULAR_EVENT = {
@@ -35,8 +27,8 @@ const SPECIAL_EVENT = {
     color: "#FF0000", // Red
 }
 
-// UCSD's Canvas infrastructure
-const coursesURL = "https://canvas.ucsd.edu/api/v1/courses?per_page=100";
+// URL to grab only currently active courses for the user
+const activeCourses = "https://canvas.ucsd.edu/api/v1/courses?enrollment_state=active";
 
 // Syntax string to insert a new event into our database
 let INSERT = 
@@ -140,9 +132,7 @@ module.exports = async function (queryUsername, queryAPIToken) {
             // Handling special events
             let isSpecial = isSpecialEvent(name);
 
-            console.log(isSpecial);
             if (isSpecial) {
-                console.log("SPECIAL EVENT DETECTED!");
                 type = "exam";
                 color = SPECIAL_EVENT.color;
             }
@@ -210,81 +200,30 @@ function isSpecialEvent(input) {
         input.toLowerCase().includes(event.toLowerCase()));
 }
 
-/* Given the user's Canvas API token, we want to fetch all of their still
- * active courses from UCSD's Canvas API. Usually the json data returned from
- * the Canvas API includes a link to an ics file, which contains all the calendar info
- * for that particular courses. 
+/* Given a valid API Token, we fetch all the related info of still active
+ * courses for the user. Among these info is a link to the ics file which is what
+ * we are interested about. 
  * 
- * PRECONDITION: apiToken is set and checked if invalid or not.
+ * PRECONDITION: apiToken is not null and valid.
  *  
  * @param No param needed
- * @return An array of course OBJECTS if the user is valid and authorized.
- *         An empty array if no courses for the user is found.
- *         An error message from Canvas otherwise.
- */
-async function getAllCourses() {
-    let options = {
-        method: 'GET',
-        headers: {
-            Authorization: 'Bearer ' + apiToken
-        },
-    }
-
-    try {
-        let courses = await fetch(coursesURL, options);
-    
-        // courseData is an Array of Objects
-        // Parse into a JSON format for human readability
-        let courseData = await courses.json(); 
-        return courseData;
-    }
-    catch (err) {
-        console.log(err.message);
-    }
-} 
-
-/* Given that we have fetched all "active" courses from Canvas,
- * which are courses that did not end and still accessible 
- * even though the class was already finished. We want to sort them
- * and grab only the ones that are still "truly active" from all 
- * the courses.
- * 
- * PRECONDITION: getAllCourses() was called to populate var data.
- *               var data is not null or undefined.
- *               (recommended) var data is not empty.
- *  
- * @param No param needed
- * @return An array of "truly active" course OBJECTS if sorted successfully.
- *         An empty array if no 'truly active" courses.
+ * @return An array of "active" course OBJECTS.
+ *         An empty array if no currently "active" courses.
  *         An error message from Canvas otherwise.
  */
 async function getCurrentCourses() {
     try {
-        data = await getAllCourses(); // Grab parsed data of ALL courses
-        let currentCourses = []; // Array to store current sources
-        let coursesDate = []; // Array to store created_at dates of ALL courses
-
-        // Isolating the 'created_at' property, make Date() OBJECTS out of them 
-        // and push them to a coursesDate array
-        // The property is in ISO8601 date format (look it up)
-        for (let i = 0; i < data.length; i++) {
-            coursesDate.push(new Date(data[i]['created_at']));
+        let options = {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + apiToken
+            },
         }
 
-        for (let i = 0; i < data.length; i++) {
-            // Get difference between today's Date and course's created at Date
-            // (in millisecond)
-            let diffTime = Math.abs(todayDate - coursesDate[i]); 
-
-            // Get said difference but in days
-            let diffDay = Math.ceil(diffTime / milliInDay); 
-
-            // A quarter at UCSD has on average 150 days, we only want
-            // courses that are still active within the last 150 days     
-            if (diffDay < daysInQuarter) {
-                currentCourses.push(data[i]);
-            }
-        }
+        // courseData is an Array of Objects
+        // Parse into a JSON format for human readability
+        let courses = await fetch(activeCourses, options);
+        let currentCourses = await courses.json(); 
 
         return currentCourses;
     }
@@ -293,7 +232,7 @@ async function getCurrentCourses() {
     }
 }
 
-/* Given that we have fetched the "truly active" courses from Canvas,
+/* Given that we have fetched the "active" courses from Canvas,
  * we want to parse the ics file included in each course object to extract
  * the calendar info from them. Currently, it is unknown which standard
  * Canvas used for their ics file. But we will just hand it to node-module
