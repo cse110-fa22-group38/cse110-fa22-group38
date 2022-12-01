939dc26ad3ed2f35f507cec8729876dae3c4c4b5
Jason011125 <zic022@ucsd.edu>
2022-12-01 10:27:00 -0800

    jsdoc for variables

--- node-express/server.js
+++ node-express/server.js
@@ -1,5 +1,13 @@
+/**
+ * initialized app by importing main.js
+ */
 const app = require("./main.js");
+/**
+ * initialize port to 6900
+ */
 const PORT = 6900;
+
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + 6900)
